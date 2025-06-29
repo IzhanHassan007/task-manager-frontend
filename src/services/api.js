bbc762a7eec3855f2f70befac22c33dd0cda2987
Syed Izhan Hassan <izhanhassan007@gmail.com>
2025-06-29 16:46:35 +0500

    .env set

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+// src/services/api.js
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true, 
 });
 
 export default api;
